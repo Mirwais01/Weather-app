9c25b722db5fdc2973c8783c933c830813e232b7
Mirwais <mirwais.sanaie@gmail.com>
2025-01-18 23:50:05 +0430

    navigation updated completly

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,181 +1,247 @@
 import { NavLink } from "react-router";
+import { useState } from "react";
 
 function Navigation() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <NavLink to={"/"} className="flex items-center">
-          <img
-            src="icons8-weather-100.png"
-            className="h-8 mr-3"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Weather app
-          </span>
-        </NavLink>
-
-        <div className="flex md:order-2">
-          <button
-            type="button"
-            data-collapse-toggle="navbar-search"
-            aria-controls="navbar-search"
-            aria-expanded="false"
-            className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
-          >
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+    <>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <div className="flex items-center md:flex-1">
+            <NavLink to={"/"} className="flex items-center">
+              <img
+                src="icons8-weather-100.png"
+                className="h-4 sm:h-6 md:h-8 mr-3"
+                alt="Weather App Logo"
               />
-            </svg>
-            <span className="sr-only">Search</span>
-          </button>
+              <span className="self-center text-sm sm:text-lg md:text-2xl font-semibold whitespace-nowrap dark:text-white">
+                Weather App
+              </span>
+            </NavLink>
+          </div>
 
-          <div className="relative hidden md:block">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <div className="relative flex items-center flex-1 justify-center">
+            <div className="relative w-full max-w-[150px] sm:max-w-xs md:max-w-md">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+                <span className="sr-only">Search icon</span>
+              </div>
+              <input
+                type="text"
+                id="search-navbar"
+                className="block w-full p-2 pl-8 text-xs sm:text-sm md:pl-10 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search..."
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center md:hidden">
+            <button
+              onClick={() => setMenuOpen(!menuOpen)}
+              className="p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="mobile-menu"
+              aria-expanded={menuOpen}
+            >
+              <span className="sr-only">Toggle menu</span>
               <svg
-                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                className="w-6 h-6"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
-                viewBox="0 0 20 20"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
               >
                 <path
-                  stroke="currentColor"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  d="M4 6h16M4 12h16m-7 6h7"
                 />
               </svg>
-              <span className="sr-only">Search icon</span>
-            </div>
-            <input
-              type="text"
-              id="search-navbar"
-              className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search..."
-            />
+            </button>
           </div>
-          <button
-            data-collapse-toggle="navbar-search"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-search"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
+
+          <div className="hidden md:flex justify-end flex-1">
+            <button className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800">
+              Login
+            </button>
+          </div>
         </div>
 
-        <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-search"
-        >
-          <div className="relative mt-3 md:hidden">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-            </div>
-            <input
-              type="text"
-              id="search-navbar"
-              className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search..."
-            />
-            <button className="my-button text-white">Button</button>
+        {menuOpen && (
+          <div className="md:hidden bg-gray-100 dark:bg-gray-800">
+            <ul className="flex flex-col items-center space-y-4 py-4 font-medium text-gray-700 dark:text-gray-300">
+              <li>
+                <NavLink
+                  to={"/"}
+                  className={({ isActive }) =>
+                    `hover:text-blue-700 dark:hover:text-blue-500 ${
+                      isActive ? "border-b-2 border-white" : ""
+                    }`
+                  }
+                  aria-current="page"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/today"}
+                  className={({ isActive }) =>
+                    `hover:text-blue-700 dark:hover:text-blue-500 ${
+                      isActive ? "border-b-2 border-white" : ""
+                    }`
+                  }
+                >
+                  Today
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/hourly"}
+                  className={({ isActive }) =>
+                    `hover:text-blue-700 dark:hover:text-blue-500 ${
+                      isActive ? "border-b-2 border-white" : ""
+                    }`
+                  }
+                >
+                  Hourly
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/tenDay"}
+                  className={({ isActive }) =>
+                    `hover:text-blue-700 dark:hover:text-blue-500 ${
+                      isActive ? "border-b-2 border-white" : ""
+                    }`
+                  }
+                >
+                  10Day
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/weekend"}
+                  className={({ isActive }) =>
+                    `hover:text-blue-700 dark:hover:text-blue-500 ${
+                      isActive ? "border-b-2 border-white" : ""
+                    }`
+                  }
+                >
+                  Weekend
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/monthly"}
+                  className={({ isActive }) =>
+                    `hover:text-blue-700 dark:hover:text-blue-500 ${
+                      isActive ? "border-b-2 border-white" : ""
+                    }`
+                  }
+                >
+                  Monthly
+                </NavLink>
+              </li>
+            </ul>
           </div>
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <NavLink
-                to={"/"}
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/today"}
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Today
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/hourly"}
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Hourly
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/tenDay"}
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                10Day
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/weekend"}
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Weekend
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/monthly"}
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Monthly
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+        )}
+      </nav>
+
+      <div className="hidden md:flex bg-gray-100 dark:bg-gray-800 h-[40px]">
+        <ul className="flex items-center  mx-auto justify-center space-x-8 font-medium text-gray-700 dark:text-gray-300">
+          <li>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                `hover:text-blue-700 dark:hover:text-blue-500 ${
+                  isActive ? "border-b-2 border-white" : ""
+                }`
+              }
+              aria-current="page"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/today"}
+              className={({ isActive }) =>
+                `hover:text-blue-700 dark:hover:text-blue-500 ${
+                  isActive ? "border-b-2 border-white" : ""
+                }`
+              }
+            >
+              Today
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/hourly"}
+              className={({ isActive }) =>
+                `hover:text-blue-700 dark:hover:text-blue-500 ${
+                  isActive ? "border-b-2 border-white" : ""
+                }`
+              }
+            >
+              Hourly
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/tenDay"}
+              className={({ isActive }) =>
+                `hover:text-blue-700 dark:hover:text-blue-500 ${
+                  isActive ? "border-b-2 border-white" : ""
+                }`
+              }
+            >
+              10Day
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/weekend"}
+              className={({ isActive }) =>
+                `hover:text-blue-700 dark:hover:text-blue-500 ${
+                  isActive ? "border-b-2 border-white" : ""
+                }`
+              }
+            >
+              Weekend
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/monthly"}
+              className={({ isActive }) =>
+                `hover:text-blue-700 dark:hover:text-blue-500 ${
+                  isActive ? "border-b-2 border-white" : ""
+                }`
+              }
+            >
+              Monthly
+            </NavLink>
+          </li>
+        </ul>
       </div>
-    </nav>
+    </>
   );
 }
 
